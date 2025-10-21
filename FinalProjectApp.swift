0bb3e47c6c28e04528a95acfeda0992cf4bc27fb
Krystal Servin <79737887+kservin4@users.noreply.github.com>
2025-10-20 22:00:55 -0500

    Added files from local file system

---
+++ FinalProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinalProjectApp.swift
+//  FinalProject
+//
+//  Created by user270281 on 5/5/25.
+//
+
+import SwiftUI
+
+@main
+struct FinalProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
